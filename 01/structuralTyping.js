53156069e6cfb741be6e870e209215772990c87e
早津渉 <top.men09@gmail.com>
2019-02-11 22:40:19 +0900

    Null vs. Undefinedまで

--- 01/structuralTyping.js
+++ 01/structuralTyping.js
@@ -5,4 +5,6 @@ function iTakePoint2D(point) {
 }
 iTakePoint2D(point2D);
 iTakePoint2D(point3D);
-iTakePoint2D({ x: 0 });
+// iTakePoint2D({x: 0}) // error
+var foo = 123;
+foo = '456'; // Error: cannot assign a `string` to a `number`
