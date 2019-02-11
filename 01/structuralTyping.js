38c40df0d12c5ce7cfd825bcbc5614c9b298ec4a
早津渉 <top.men09@gmail.com>
2019-02-11 22:21:22 +0900

    first commit

---
+++ 01/structuralTyping.js
@@ -0,0 +1,8 @@
+var point2D = { x: 0, y: 10 };
+var point3D = { x: 0, y: 10, z: 20 };
+function iTakePoint2D(point) {
+    console.log(point);
+}
+iTakePoint2D(point2D);
+iTakePoint2D(point3D);
+iTakePoint2D({ x: 0 });
