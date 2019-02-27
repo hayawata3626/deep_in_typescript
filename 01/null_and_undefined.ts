4d46c7ebc7576ed96ebf224139ec1fe97691c2cc
早津渉 <top.men09@gmail.com>
2019-02-28 00:00:39 +0900

    voidを追加

---
+++ 01/null_and_undefined.ts
@@ -0,0 +1,8 @@
+var num: number;
+var str: string;
+
+// These literals can be assigned to anything
+num = null;
+str = undefined;
+console.log(num)
+console.log(str)
---
+++ 01/void.ts
@@ -0,0 +1,5 @@
+// void: 関数に戻り値がないことを示すには:voidを使います
+
+function log(message):void {
+  console.log(message)
+}
