7e878c693d83ce51d8431b263ee321ed7e0b81d1
早津渉 <top.men09@gmail.com>
2019-03-10 13:03:19 +0900

    overload

--- 01/functions.ts
+++ 01/functions.ts
@@ -7,3 +7,29 @@ function foo(sample: Foo): Foo {
 }
 
 console.log(foo({foo: "ほげほげ"}))
+
+
+function padding(all: number);
+function padding(topAndBottom: number, leftAndRight: number);
+function padding(top: number, right: number, bottom: number, left: number);
+
+function padding(a: number, b?: number, c?: number, d?: number) {
+  if (b === undefined && c === undefined && d === undefined) {
+    b = c = d = a;
+  }
+  else if (c === undefined && d === undefined) {
+    c = a;
+    d = b;
+  }
+  return {
+    top: a,
+    right: b,
+    bottom: c,
+    left: d
+  };
+}
+
+padding(1); // Okay: all
+padding(1,1); // Okay: topAndBottom, leftAndRight
+padding(1,1,1,1); // Okay: top, right, bottom, left
+padding(1,1,1); // Error: Not a part of the available overloads
